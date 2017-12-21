a1f3731966f2011fad318e99dbcdf10fb6a523ea
Nacho L <nacho@nlopez.io>
2017-12-21 12:40:18 +0000

    Removed bottom tabs

--- sample/src/main/java/io/nlopez/smartlocation/sample/MainActivity.java
+++ sample/src/main/java/io/nlopez/smartlocation/sample/MainActivity.java
@@ -1,45 +1,17 @@
 package io.nlopez.smartlocation.sample;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager mPager;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_location:
-                    mPager.setCurrentItem(0);
-                    return true;
-                case R.id.navigation_geocoding:
-                    mPager.setCurrentItem(1);
-                    return true;
-                case R.id.navigation_geofencing:
-                    mPager.setCurrentItem(2);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPager = findViewById(R.id.container);
-        mPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
-
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final ViewPager pager = findViewById(R.id.container);
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
     }
-
 }
